8af1e9c99107c019b8dfe42dacfd539a8b0a86a2
NikDS <NikerinDS@gmail.com>
2025-09-01 12:55:35 +0500

    Создан класс Task для простых задач

---
+++ src/Task.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+public class Task {
+    private static int newId = 1;
+
+    private int id;
+    private String name;
+    private String description;
+    private TaskStatus status;
+
+    public Task(int id, String name, String description, TaskStatus status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Task(String name, String description) {
+        this.id = newId++;
+        this.name = name;
+        this.description = description;
+        this.status = TaskStatus.NEW;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
